46e26238c1b387cadf5116e7725ba7ecb4e43203
Giuseppe-Gambino <pippo.gmb18@gmail.com>
2025-01-30 10:34:25 +0100

    installazione libreria/plugin daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,13 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("daisyui"),
+  ],
+  daisyui: {
+    themes: ["sunset", "fantasy"], // Scegli i temi disponibili
+    darkTheme: "sunset", // Tema predefinito per dark mode
+  },
 };
