afbc6477ac306c5ad63de6106402869d8d563999
Giuseppe-Gambino <pippo.gmb18@gmail.com>
2025-02-09 12:04:03 +0100

    small css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        sm: "0.750rem",
+        base: "1rem",
+        xl: "1.333rem",
+        "2xl": "1.777rem",
+        "3xl": "2.369rem",
+        "4xl": "3.158rem",
+        "5xl": "4.210rem",
+      },
+      fontFamily: {
+        heading: "Inter",
+        body: "Inter",
+      },
+      fontWeight: {
+        normal: "400",
+        bold: "700",
+      },
+    },
     container: {
       center: true,
       padding: "1rem",
@@ -25,7 +43,7 @@ module.exports = {
         darktheme: {
           primary: "#CAF7ED",
           secondary: "#202431",
-          accent: "#3F6C8D",
+          accent: "#0093FF",
           neutral: "#171A26",
           "base-100": "#171A26",
           info: "#8DA7C3",
