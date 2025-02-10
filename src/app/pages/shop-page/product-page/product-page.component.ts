c9d750687db37e229ce37d58e1a5dd70b305da5e
Giuseppe-Gambino <pippo.gmb18@gmail.com>
2025-02-10 22:13:45 +0100

    grafico prezzi

--- src/app/pages/shop-page/product-page/product-page.component.ts
+++ src/app/pages/shop-page/product-page/product-page.component.ts
@@ -19,6 +19,8 @@ export class ProductPageComponent implements OnInit {
   reseller!: iUser;
   length: number = 0;
 
+  storicoPrezzi: any;
+
   count: number = 1;
 
   constructor(
@@ -27,6 +29,8 @@ export class ProductPageComponent implements OnInit {
     private cartSvc: CartService,
     private route: ActivatedRoute
   ) {}
+
+  chartOptions: any;
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id')!;
@@ -37,6 +41,10 @@ export class ProductPageComponent implements OnInit {
         switchMap((product) => {
           this.product = product;
           this.imgPreview = product.imageUrls[0];
+          this.storicoPrezzi = product.priceHistory;
+          console.log(this.storicoPrezzi);
+
+          this.grafico();
           console.log(product);
 
           return this.userSvc.getById(product.resellerId);
@@ -52,6 +60,27 @@ export class ProductPageComponent implements OnInit {
         this.length = result;
       }
     });
+  }
+
+  grafico() {
+    // Esempio di dati con date: prezzo
+    const priceHistory = this.storicoPrezzi;
+
+    const labels = Object.keys(priceHistory);
+    const data = Object.values(priceHistory);
+
+    this.chartOptions = {
+      tooltip: { trigger: 'axis' },
+      xAxis: { type: 'category', data: labels },
+      yAxis: { type: 'value' },
+      series: [
+        {
+          data,
+          type: 'line',
+          smooth: true,
+        },
+      ],
+    };
   }
 
   imgSwitch(img: string) {
--- src/app/pages/shop-page/shop-page.module.ts
+++ src/app/pages/shop-page/shop-page.module.ts
@@ -1,13 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NgxEchartsModule } from 'ngx-echarts';
+import * as echarts from 'echarts/core';
+import { LineChart } from 'echarts/charts';
+import { CanvasRenderer } from 'echarts/renderers';
+import { TooltipComponent, GridComponent } from 'echarts/components';
+
 import { ShopPageRoutingModule } from './shop-page-routing.module';
 import { ShopPageComponent } from './shop-page.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { FormsModule } from '@angular/forms';
 
+echarts.use([LineChart, CanvasRenderer, TooltipComponent, GridComponent]);
+
 @NgModule({
   declarations: [ShopPageComponent, ProductPageComponent],
-  imports: [CommonModule, ShopPageRoutingModule, FormsModule],
+  imports: [
+    CommonModule,
+    ShopPageRoutingModule,
+    FormsModule,
+    NgxEchartsModule.forRoot({
+      echarts,
+    }),
+  ],
 })
 export class ShopPageModule {}
